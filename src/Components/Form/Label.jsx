c760dfd400e60ee48af39e3b19cb6dd3a3a1fd5b
Abhishek Patel <abhishekpatel946@gmail.com>
2021-06-10 10:16:08 +0530

    fix: handle PropTypes checking
    strictly checking the label name in each component using proptype

--- src/Components/Form/Label.jsx
+++ src/Components/Form/Label.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './css/Form-style.css';
 
 const Label = (props) => {
+  // typechecking with propTypes
+  Label.propTypes = {
+    label: PropTypes.string,
+  };
+
   return (
     <div className='Label'>
       <label>{props.label}</label>
